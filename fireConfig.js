9989ad0156e6ff0254e98d5de9af20e1444b2145
KylaLazaro <144894471+KylaLazaro@users.noreply.github.com>
2024-06-12 22:02:45 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDnlYx28_Rrt1U8SqKwdhpKUT4cIeXJpKI",
+    authDomain: "crud-f7d01.firebaseapp.com",
+    databaseURL: "https://crud-f7d01-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "crud-f7d01",
+    storageBucket: "crud-f7d01.appspot.com",
+    messagingSenderId: "927110859868",
+    appId: "1:927110859868:web:f6a2dd70592d96a9d3aa99"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
